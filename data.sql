67884a0df8b7c7d8755b06e0b4a8942d228acae9
HelloSolo <davidalimi148@gmail.com>
2022-09-21 20:44:05 +0100

    created dummy data

---
+++ data.sql
@@ -0,0 +1,5 @@
+insert into movie_genre (name)
+values ("Action"),("Thriller"),("Romance"), ("Comedy");
+
+insert into movie_genre (title, genre, numberInStock, dailyRentalRate)
+values ("Airplanes", 1, 10, 3), ("Airplanes", 1, 10, 3)
