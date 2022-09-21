1482ba40cfef05012fa93ae4ef3262bbe866385e
HelloSolo <davidalimi148@gmail.com>
2022-09-21 16:11:54 +0100

    added movie and genre models

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.ForeignKey(Genre, on_delete=models.PROTECT)
+    numberInStock = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(100)]
+    )
+    dailyRentalRate = models.DecimalField(
+        max_digits=3,
+        decimal_places=2,
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+    )
--- vidly_api/settings.py
+++ vidly_api/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
-    "playground"
+    "playground",
+    "movies",
 ]
 
 MIDDLEWARE = [
