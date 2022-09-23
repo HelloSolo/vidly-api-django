5f177303f514b67b037a8fbd60a2d2aec4cb1fc3
HelloSolo <davidalimi148@gmail.com>
2022-09-23 11:41:39 +0100

    added saving functionality

--- movie/views.py
+++ movie/views.py
@@ -1,23 +1,39 @@
-from itertools import product
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Movie
 from .serializers import MovieSerializer
 
 # Create your views here.
-@api_view()
+@api_view(["GET", "POST"])
 def movie_list(request):
-    queryset = Movie.objects.select_related("genre").all()
-    serializer = MovieSerializer(queryset, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        queryset = Movie.objects.select_related("genre").all()
+        serializer = MovieSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = MovieSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view()
+@api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
-    movie = Movie.objects.get(pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    movie = get_object_or_404(Movie, pk=pk)
+
+    if request.method == "GET":
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = MovieSerializer(movie, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "DELETE":
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
