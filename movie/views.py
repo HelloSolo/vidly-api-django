b63765ce183de5a6b2c0a4b0afaf461044bfea2f
HelloSolo <davidalimi148@gmail.com>
2022-09-24 15:19:12 +0100

    fixed pagination errors

--- movie/views.py
+++ movie/views.py
@@ -1,15 +1,18 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from .models import Movie, Genre
 from .serializers import AddMovieSerializer, MovieSerializer, GenreSerializer
 from .filter import MovieFilter
+from .paginate import DefaultPagination
 
 
 class MovieViewSet(ModelViewSet):
     queryset = Movie.objects.select_related("genre").all()
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = MovieFilter
+    pagination_class = DefaultPagination
     ordering_fields = ["dailyRentalRate", "numberInStock"]
     search_fields = ["title"]
 
@@ -17,6 +20,11 @@ class MovieViewSet(ModelViewSet):
         if self.request.method == "POST" or self.request.method == "PUT":
             return AddMovieSerializer
         return MovieSerializer
+
+    def paginate_queryset(self, queryset):
+        if self.request.headers["Host"] == "localhost:8000":
+            return None
+        return super().paginate_queryset(queryset)
 
 
 class GenreViewSet(ReadOnlyModelViewSet):
