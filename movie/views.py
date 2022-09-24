8c4b6f6bf7057d21a60ac149b3980155a68eec8e
HelloSolo <davidalimi148@gmail.com>
2022-09-24 01:53:51 +0100

    added searching and sorting functionality

--- movie/views.py
+++ movie/views.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from .models import Movie, Genre
 from .serializers import MovieSerializer, GenreSerializer
@@ -8,8 +9,10 @@ from .filter import MovieFilter
 class MovieViewSet(ModelViewSet):
     queryset = Movie.objects.select_related("genre").all()
     serializer_class = MovieSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = MovieFilter
+    search_fields = ["title"]
+    ordering_fields = ["dailyRentalRate", "numberInStock"]
 
 
 class GenreViewSet(ReadOnlyModelViewSet):
