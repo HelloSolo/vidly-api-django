013aec4e513ab4ff2973ccdf012d3f109729b766
HelloSolo <davidalimi148@gmail.com>
2022-09-24 03:52:46 +0100

    add more data

--- data.sql
+++ data.sql
@@ -2,4 +2,14 @@ insert into movie_genre (name)
 values ("Action"),("Thriller"),("Romance"), ("Comedy");
 
 insert into movie_genre (title, genre, numberInStock, dailyRentalRate)
-values ("Airplanes", 1, 10, 3), ("Airplanes", 1, 10, 3)
+values 	("Airplanes", 1, 10, 3.0),
+		("Die Hard", 1, 20, 7.0),
+        ("Gone Girl", 2, 7, 4.0),
+        ("How To Train A Dragon", 2, 10, 7.0),
+        ("Lover", 3, 67, 8.0),
+        ("No Time To Die", 2, 67, 8.9),
+        ("Run  Aways", 2, 33, 5.6),
+        ("Terminator 3", 1, 79, 9.0),
+        ("The Avengers", 2, 5, 9.0),
+        ("The Big Bang Theory", 4, 5, 8.9),
+        ("The Hangover", 4, 8, 2)
