48320da021b0c82ef621af338d90cc8c555c9eb7
HelloSolo <davidalimi148@gmail.com>
2022-09-24 15:21:15 +0100

    ...

--- movie/views.py
+++ movie/views.py
@@ -1,5 +1,4 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.response import Response
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from .models import Movie, Genre
