234b196bdcb5e690b40196805158ec4039ab9ad2
HelloSolo <davidalimi148@gmail.com>
2022-09-26 21:05:20 +0100

    created nested url for movie images

--- movie/urls.py
+++ movie/urls.py
@@ -1,13 +1,13 @@
-from xml.etree.ElementInclude import include
 from django.urls import path, include
-from .views import GenreViewSet, MovieViewSet
-from rest_framework.routers import DefaultRouter
+from .views import GenreViewSet, MoviePosterViewSet, MovieViewSet
+from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 
 
 router = DefaultRouter()
 router.register("movies", MovieViewSet, basename="movie")
 router.register("genres", GenreViewSet)
 
-urlpatterns = [
-    path("", include(router.urls)),
-]
+movie_router = NestedDefaultRouter(router, "movies", lookup="movie")
+movie_router.register("images", MoviePosterViewSet, basename="movie-images")
+
+urlpatterns = [path("", include(router.urls)), path("", include(movie_router.urls))]
