a60d14ef3c424939cdcd9cbf9d8a2a66137dfca8
HelloSolo <davidalimi148@gmail.com>
2022-09-27 02:02:06 +0100

    added inlines for movies poster to admin page

--- movie/admin.py
+++ movie/admin.py
@@ -2,15 +2,26 @@ from django.contrib import admin
 from django.db.models import Count
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
-from .models import Movie, Genre
+from .models import Movie, Genre, MoviePoster
 
 # Register your models here.
+
+
+class ImagesInline(admin.TabularInline):
+    model = MoviePoster
+    readonly_fields = ["thumbnail"]
+
+    def thumbnail(self, instance):
+        if instance.image.name != "":
+            return format_html(f'<img src="{instance.image.url}" class="thumbnail">')
+        return ""
 
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     actions = ["clear_stock"]
     autocomplete_fields = ["genre"]
+    inlines = [ImagesInline]
     list_display = ["title", "genre", "numberInStock", "dailyRentalRate"]
     list_editable = ["numberInStock", "dailyRentalRate"]
     list_filter = ["genre"]
@@ -23,6 +34,9 @@ class MovieAdmin(admin.ModelAdmin):
     def clear_stock(self, request, queryset):
         update_count = queryset.update(numberInStock=0)
         self.message_user(request, f"{update_count} movies has been updated")
+
+    class Media:
+        css = {"all": ["movie/styles.css"]}
 
 
 @admin.register(Genre)
