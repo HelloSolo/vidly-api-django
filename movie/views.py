71670afd977262709490ed31ba3353698134ed3b
HelloSolo <davidalimi148@gmail.com>
2022-10-05 22:31:03 +0100

    Added permissions to watchlist & customer viewset

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet, GenericViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet, GenericViewSet
 from .models import Customer, Movie, Genre, MoviePoster, WatchList
 from .serializers import (
     AddMovieSerializer,
@@ -57,6 +58,7 @@ class CustomerViewSet(
 ):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated]
 
     @action(detail=False, methods=["GET", "PUT"])
     def me(self, request):
@@ -72,6 +74,8 @@ class CustomerViewSet(
 
 
 class WatchListViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
+
     def get_queryset(self):
         return WatchList.objects.select_related("movie").filter(
             customer_id=self.kwargs["customer_pk"]
