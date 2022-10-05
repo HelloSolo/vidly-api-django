d078867f8541cee777cb9adb6dd3f741405e9f0e
HelloSolo <davidalimi148@gmail.com>
2022-10-05 21:55:38 +0100

    added signals from creating customers

--- movie/apps.py
+++ movie/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class MovieConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "movie"
+
+    def ready(self) -> None:
+        import movie.signals
+
+        return super().ready()
