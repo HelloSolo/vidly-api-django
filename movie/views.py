ced800e0508f7fe837e9ca22f58b322f04edcc27
HelloSolo <davidalimi148@gmail.com>
2022-10-05 21:44:22 +0100

    adjusted customer viewset

--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,9 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet, GenericViewSet
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
 from .models import Customer, Movie, Genre, MoviePoster, WatchList
 from .serializers import (
     AddMovieSerializer,
@@ -48,9 +51,23 @@ class GenreViewSet(ReadOnlyModelViewSet):
     serializer_class = GenreSerializer
 
 
-class CustomerViewSet(ModelViewSet):
-    queryset = Customer.objects.select_related("user").all()
+class CustomerViewSet(
+    CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet
+):
+    queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+    @action(detail=False, methods=["GET", "PUT"])
+    def me(self, request):
+        (customer, created) = Customer.objects.get(user_id=request.user.id)
+        if request.method == "GET":
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        elif request.method == "PUT":
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
 
 
 class WatchListViewSet(ModelViewSet):
