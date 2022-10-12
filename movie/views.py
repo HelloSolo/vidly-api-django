5e3467d35280aa39c59ad550e37bfac43d9718b5
HelloSolo <davidalimi148@gmail.com>
2022-10-12 15:32:22 +0100

    added viewset for subscriptions

--- movie/views.py
+++ movie/views.py
@@ -66,16 +66,11 @@ class CustomerViewSet(ModelViewSet):
     serializer_class = CustomerSerializer
     permission_classes = [IsAdminUser]
 
-    def get_serializer_class(self):
-        if self.request.method == "GET":
-            return CustomerSerializer
-        return AddCustomerSerializer
-
     @action(detail=False, methods=["GET", "PUT"], permission_classes=[IsAuthenticated])
     def me(self, request):
         customer = Customer.objects.get(user_id=request.user.id)
         if request.method == "GET":
-            serializer = AddCustomerSerializer(customer)
+            serializer = CustomerSerializer(customer)
             return Response(serializer.data)
         elif request.method == "PUT":
             serializer = AddCustomerSerializer(customer, data=request.data)
