c144505a23817359e2d37f1e85db4b808e79d79e
HelloSolo <davidalimi148@gmail.com>
2022-11-30 21:40:38 +0100

    improved signalling process

--- movie/signals.py
+++ movie/signals.py
@@ -7,4 +7,5 @@ from .models import Customer, SubcriptionType
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_customer_for_new_user(sender, **kwargs):
     if kwargs["created"]:
-        Customer.objects.create(user=kwargs["instance"], subscription_id=1)
+        subcription = SubcriptionType.objects.get(plan="Free")
+        Customer.objects.create(user=kwargs["instance"], subscription_id=subcription.id)
