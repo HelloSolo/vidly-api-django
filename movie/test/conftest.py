e5a287dbd044b88ecef9309602fbad4ca9577b75
HelloSolo <davidalimi148@gmail.com>
2022-11-30 21:40:54 +0100

    pytest setup

--- movie/test/conftest.py
+++ movie/test/conftest.py
@@ -1,7 +1,22 @@
 from rest_framework.test import APIClient
+from core.models import User
+from movie.models import SubcriptionType
+from model_bakery import baker
 import pytest
 
 
 @pytest.fixture
 def api_client():
-    return APIClient()
+    client = APIClient()
+    return client
+
+
+@pytest.fixture
+def authenticate(api_client):
+    SubcriptionType.objects.all().delete()
+    User.objects.all().delete()
+    client = api_client
+    baker.make(SubcriptionType, plan="Free")
+    user = baker.make(User)
+    client.force_authenticate(user=user)
+    return client
