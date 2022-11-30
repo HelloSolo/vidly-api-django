50baab9c9755c705e3b4fd001fbdf42d7cb6ec29
HelloSolo <davidalimi148@gmail.com>
2022-11-30 21:40:13 +0100

    changes display string display for User model

--- core/models.py
+++ core/models.py
@@ -5,5 +5,8 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True)
 
+    # def __str__(self) -> str:
+    #     return f"{self.first_name} {self.last_name}"
+
     def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.username}"
