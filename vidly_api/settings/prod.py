5874a8756df3473b6d81b34be2c90900a897e95f
HelloSolo <davidalimi148@gmail.com>
2022-12-20 12:36:44 +0100

    ...

--- vidly_api/settings/prod.py
+++ vidly_api/settings/prod.py
@@ -5,15 +5,4 @@ DEBUG = os.getenv("DEBUG")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": os.getenv("ENGINE"),
-#         "NAME": os.getenv("NAME"),
-#         "HOST": os.getenv("HOST"),
-#         "USER": os.getenv("USER"),
-#         "PASSWORD": os.getenv("PASSWORD"),
-#         "PORT": os.getenv("PORT"),
-#     }
-# }
-
 DATABASES = {"default": dj_database_url.config()}
