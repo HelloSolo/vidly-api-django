cd4c7c3a588219cf513c3ae1f35d511f97c348da
HelloSolo <davidalimi148@gmail.com>
2022-12-20 17:25:36 +0100

    added STATICFILES_STORAGE

--- vidly_api/settings/dev.py
+++ vidly_api/settings/dev.py
@@ -1,27 +1,29 @@
 from .common import *
 
 
-DEBUG = True
+DEBUG = False
 
 SECRET_KEY = "django-insecure-ma^cg$qf9ly-^4&eomakqqu*b6)%y3^^mw(**ct*hfbisctld4"
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "vidly",
-        "HOST": "127.0.0.1",
-        "USER": "root",
-        "PASSWORD": "Underw@ter",
-    }
-}
 
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "railway",
-#         "HOST": "containers-us-west-172.railway.app",
+#         "NAME": "vidly",
+#         "HOST": "127.0.0.1",
 #         "USER": "root",
-#         "PASSWORD": "XrL6dzrgdKAzmCQnnME5",
-#         "PORT": "7862",
+#         "PASSWORD": "Underw@ter",
 #     }
 # }
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "railway",
+        "HOST": "containers-us-west-174.railway.app",
+        "USER": "root",
+        "PASSWORD": "5gc1oLbcZLuprqt50eDS",
+        "PORT": "6380",
+    }
+}
