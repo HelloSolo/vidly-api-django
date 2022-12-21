d1cebd937b9c66320f019ea6bdc9299a4fe34d8c
HelloSolo <davidalimi148@gmail.com>
2022-12-21 09:59:52 +0100

    ...

--- vidly_api/settings/dev.py
+++ vidly_api/settings/dev.py
@@ -1,29 +1,16 @@
 from .common import *
 
 
-DEBUG = False
+DEBUG = True
 
 SECRET_KEY = "django-insecure-ma^cg$qf9ly-^4&eomakqqu*b6)%y3^^mw(**ct*hfbisctld4"
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "vidly",
-#         "HOST": "127.0.0.1",
-#         "USER": "root",
-#         "PASSWORD": "Underw@ter",
-#     }
-# }
-
-ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "railway",
-        "HOST": "containers-us-west-174.railway.app",
+        "NAME": "vidly",
+        "HOST": "127.0.0.1",
         "USER": "root",
-        "PASSWORD": "5gc1oLbcZLuprqt50eDS",
-        "PORT": "6380",
+        "PASSWORD": "Underw@ter",
     }
 }
