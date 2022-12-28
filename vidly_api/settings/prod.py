56119463b48b46fd39c821453ca95ab8fbe75fbb
HelloSolo <davidalimi148@gmail.com>
2022-12-28 09:28:38 +0100

    preparing for deploying django

--- vidly_api/settings/prod.py
+++ vidly_api/settings/prod.py
@@ -7,4 +7,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DATABASES = {"default": dj_database_url.config()}
 
-ALLOWED_HOSTS = ["web-production-6a07.up.railway.app", "vidly-api-ndob.onrender.com"]
+ALLOWED_HOSTS = ["web-production-6a07.up.railway.app"]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
