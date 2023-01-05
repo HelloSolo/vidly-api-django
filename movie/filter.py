05b8da5ea8b1af6d8a8187d041d3b4aa43561cb8
HelloSolo <davidalimi148@gmail.com>
2023-01-05 10:26:00 +0100

    ...

--- movie/filter.py
+++
@@ -1,8 +0,0 @@
-import django_filters
-from .models import Movie
-
-
-class MovieFilter(django_filters.FilterSet):
-    class Meta:
-        model = Movie
-        fields = {"genre_id": ["exact"], "imdbRating": ["gt", "lt"]}
